c7aff2a18e8c0de9ebe3c9f22a3421c22d1c7cd6
ANKIT82529490 <146726829+ANKIT82529490@users.noreply.github.com>
2026-02-18 17:00:09 +0530

    fix build error

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-  server:{port:5173}
-})
+  plugins: [react()],
+  build: {
+    cssMinify: "esbuild",
+  },
+});
