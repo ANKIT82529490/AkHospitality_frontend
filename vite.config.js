074a3d93f4252b058df6ed2bca6484e37d6d85a3
ANKIT82529490 <146726829+ANKIT82529490@users.noreply.github.com>
2026-02-18 17:17:17 +0530

    fix build error

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    cssMinify: "esbuild",
+    chunkSizeWarningLimit: 1000, // 500 -> 1000kb
   },
 });
